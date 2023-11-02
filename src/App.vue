578ead336b812be1a52bffd2fde7d9f837e22f85
Kovács László <lacikovacs330@gmail.com>
2023-11-02 15:54:17 +0100

    Nav

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Nav from './components/Nav.vue';
+
+</script>
+
+<template>
+  <header><Nav></Nav></header>
+  <main></main>
+  <footer></footer>
+</template>
+
+<style>
+
+
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,216 @@
+<script setup>
+</script>
+
+<template>
+  <nav>
+    <div class="nav-logo">
+      <img src="../assets/web.png" alt="logo">
+    </div>
+    <div class="nav-texts">
+      <div class="nav-upper">
+        <div class="nav-upper-left">
+          <span><fa icon="phone" style="color: #6E6F71; width: 15px; height: 15px; margin-right: 8px;" />+36 1 123 4567</span>
+          <span><fa icon="envelope" style="color: #6E6F71; width: 15px; height: 15px; margin-right: 8px; margin-left: 20px;" />info@netipar.hu</span>
+        </div>
+        <div class="nav-upper-center">
+          <fa :icon="['fab', 'facebook']" style="color: #6E6F71;" />
+          <fa :icon="['fab', 'linkedin']" style="color: #6E6F71; margin-left: 15px;" />
+        </div>
+        <div class="nav-upper-right">
+          <div class="nav-upper-right-search">
+            <div class="icon-container">
+              <fa :icon="['fas', 'magnifying-glass']" style="color: gray;" class="icon" />
+            </div>
+            <input type="text" class="input-field" />
+          </div>
+          <div class="nav-upper-right-language">
+            <span style="color: #fff;">Magyar</span>
+            <fa :icon="['fas', 'angle-down']" style="color: #fff; font-size: 12px; margin-left: 5px;" />
+          </div>
+        </div>
+      </div>
+      <div class="nav-lower">
+        <div class="nav-lower-buttons">
+            <button class="my-button">Rólunk</button>
+            <button class="my-button">Dokumentumok</button>
+            <button class="my-button">Tagjaink</button>
+            <button class="my-button">Csatlakozom</button>
+            <button class="my-button">Híreink</button>
+            <button class="my-button">Rendezvények</button>
+            <button class="my-button">Pályázatok</button>
+            <button class="my-button">Elemzések</button>
+            <button class="my-button">Piactér</button>
+        </div>
+        <div class="nav-lower-buttons-1">
+            <button class="my-button1" style="background-color: #03AB96; color: #fff;">Belépés</button>
+            <button class="my-button1" style="background-color: #fff; color: #03AB96;">Regisztráció</button>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style>
+h1 {
+  color: black;
+}
+
+img {
+  width: 100px;
+  height: 100px;
+}
+
+nav {
+  width: 100%;
+  height: 140px;
+  display: flex;
+  background-color: #666666;
+}
+
+.nav-logo {
+  width: 10%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.nav-texts {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.nav-upper {
+  height: 40px;
+  background-color: #000;
+  display: flex;
+}
+
+
+
+body {
+  margin: 0;
+}
+
+@import url('https://fonts.googleapis.com/css2?family=Quicksand&family=Raleway:wght@100&display=swap');
+
+.nav-upper-left {
+  width: 30%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  color: gray;
+  justify-content: center;
+}
+
+.nav-upper-center {
+  width: 40%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  color: gray;
+  justify-content: flex-end;
+}
+
+span {
+  font-family: 'Raleway', sans-serif;
+  font-size: 12px;
+}
+
+.nav-upper-right {
+  width: 29%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+}
+
+.nav-upper-right-search {
+  display: flex;
+  align-items: center;
+  padding: 5px;
+  margin-left: 20px;
+  justify-content: flex-start;
+}
+
+.icon-container {
+  position: relative;
+  display: inline-block;
+  margin-right: 10px;
+}
+
+.input-field {
+  width: 216px;
+  height: 27px;
+  background-color: #333333;
+  border: 0;
+  padding-left: 30px;
+  color: white;
+}
+
+.icon {
+  position: absolute;
+  top: 50%;
+  left: 15px;
+  transform: translateY(-50%);
+  font-size: 15px;
+}
+
+.nav-upper-right-language
+{
+    margin-left: auto;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    font-size: 10px;
+}
+
+.nav-lower {
+  width: 100%;
+  height: 80%;
+  background-color: #666666;
+  font-weight: bold;
+  display: flex;
+}
+
+.nav-lower-buttons{
+    width: 80%;
+    height: 100%;
+    margin-right: 55px;
+}
+
+.my-button {
+    height: 100%;
+    background-color: #666666;
+    color: white;
+    padding: 5px 9px;
+    border: none;
+    cursor: pointer;
+    font-size: 16px;
+    transition: background-color 0.3s;
+    text-transform: uppercase;
+    float: right;
+  }
+
+  .my-button:hover {
+    background-color: #03AB96;
+  }
+
+  .nav-lower-buttons-1{
+    width: 20%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    background-color: #03AB96;
+  }
+
+  .my-button1{
+    height: 50%;
+    border: 0;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 16px;
+  }
+</style>
